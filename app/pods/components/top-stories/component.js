fd22fa1f35fcedd7fb36df2dc3a8048155b968c8
Jason Evans <505818+jasonmevans@users.noreply.github.com>
2019-10-31 21:14:54 -0700

    :zap: Add state to the component and load stories from API

--- app/pods/components/top-stories/component.js
+++ app/pods/components/top-stories/component.js
@@ -1,3 +1,25 @@
 import Component from "@ember/component";
 
-export default class TopStories extends Component {}
+export default class TopStories extends Component {
+  // component args
+  nToLoad = 10;
+
+  // state
+  stories = [];
+
+  async init() {
+    super.init();
+
+    this.set(
+      "stories",
+      await window
+        .fetch(
+          "https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty"
+        )
+        .then(async response => {
+          return response.json();
+        })
+    );
+    console.log(this.nToLoad, this.stories);
+  }
+}
