f9bbe7c783869b41a1b5c190f3025bc563e5da86
Jason Evans <505818+jasonmevans@users.noreply.github.com>
2019-10-31 21:33:12 -0700

    :zap: Load n+1 data in individual story components

--- app/pods/components/top-stories/story/component.js
+++ app/pods/components/top-stories/story/component.js
@@ -1,4 +1,30 @@
-import Component from '@ember/component';
+import Component from "@ember/component";
+import { computed } from "@ember/object";
 
-export default Component.extend({
-});
+export default class TopStoriesStory extends Component {
+  // component args
+  id = null;
+
+  // state
+  storyData = {};
+
+  async init() {
+    super.init();
+
+    this.set(
+      "storyData",
+      await window
+        .fetch(
+          `https://hacker-news.firebaseio.com/v0/item/${this.id}.json?print=pretty`
+        )
+        .then(async response => {
+          return response.json();
+        })
+    );
+  }
+
+  @computed("storyData.date")
+  get date() {
+    return new Date(this.storyData.time).toLocaleString();
+  }
+}
